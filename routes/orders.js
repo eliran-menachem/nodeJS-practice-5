336e3c6536786459ff4279fe25e757d61a6cf8f4
eliran menachem <eliran.menachem@gmail.com>
2019-12-30 20:08:48 +0200

    orders/:id/products

--- routes/orders.js
+++ routes/orders.js
@@ -2,22 +2,52 @@ var express = require('express');
 var router = express.Router();
 
 products = [
-    {id:1,name:"HDD",price:600,weight:25},
-    {id:2,name:"Car",price:80000,weight:25000},
-    {id:3,name:"Key",price:10,weight:10},
-    {id:4,name:"Shirt",price:50,weight:6}
+  { id: 1, name: "HDD", price: 600, weight: 25 },
+  { id: 2, name: "Car", price: 80000, weight: 25000 },
+  { id: 3, name: "Key", price: 10, weight: 10 },
+  { id: 4, name: "Shirt", price: 50, weight: 6 }
 ]
 orders = [
-    {id:1,date:"10-10-2019",sum:2000,products:[]},
-    {id:2,date:"21-09-2019",sum:5550,products:[1,3,4]},
-    {id:3,date:"13-12-2019",sum:300,products:[2,3]},
-    {id:4,date:"22-02-2019",sum:4000,products:[1,2]}
+  { id: 1, date: "10-10-2019", sum: 2000, products: [3] },
+  { id: 2, date: "21-09-2019", sum: 5550, products: [1, 3, 4] },
+  { id: 3, date: "13-12-2019", sum: 300, products: [2, 3] },
+  { id: 4, date: "22-02-2019", sum: 4000, products: [1, 2] }
 ]
 
 /* GET orders page. */
-router.get('/', function(req, res, next) {
-  res.end( '[GET] orders page' );
+router.get('/', function (req, res, next) {
+  // res.send('[GET] orders page')
+  res.status(orders.length ? 200 : 204).json(orders);
 });
+
+// orders/:id
+router.get('/:id', function (req, res, next) {
+  let id = req.params.id;
+  let orderNum = orders.find((index) => index.id == id)
+  res.status(orderNum ? 200 : 204).json(orderNum);
+});
+
+
+// orders/:id/products
+router.get('/:id/products', (req, res, next) => {
+  let id = req.params.id;
+  let found = orders.find((index) => index.id == id);
+  res.status(found ? 200 : 204);
+  let result = [];
+  let productsInOrder = found.products
+  res.status(productsInOrder ? 200 : 204);
+  if (productsInOrder) {
+    // productsInOrder.forEach((element, index) => {
+    //       console.log();
+    // });
+
+    for (let i = 0; i < productsInOrder.length; i++) {
+      let p = products.find(o => o.id == found.products[i])
+      p ? result.push(p) : null
+    }
+    res.json(result)
+  }
+})
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,15 @@ router.get('/delete', function(req, res, next) {
   res.send('delete user');
 });
 
+router.post('/', function(req, res, next) {
+  console.log(req.body); // this is the parameter in body
+  console.log(req.query); // this is the parameter on URL
+  console.log(req.params);
+  res.send('[POST] users');
+});
+router.post('/:id', function(req, res, next) {
+  console.log(req.params);
+  res.send('[POST] users');
+});
+
 module.exports = router;
